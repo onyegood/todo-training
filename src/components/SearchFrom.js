3a21943aae29b050535820290f1e4932dc9727cb
Motun <motunrayodacosta@gmail.com>
2022-05-13 11:38:52 +0100

    update

--- src/components/SearchFrom.js
+++ src/components/SearchFrom.js
@@ -1,17 +1,34 @@
-import React from 'react'
+import {useState} from "react";
+import InputField from './form-elements/InputField'
 
 const SearchFrom = () => {
+  const [state, setState] = useState({
+  value:''
+});
 
+const onChange = (e) => {
+  setState({
+    ...state,
+    [e.target.id]: e.target.value,
+  });
+};
+const handleSubmitFrom = (e) => {
+  e.preventDefault();
+  console.log(state);
+};
   return (
-    <form className='search'>
-      <input
-        className='form-control m-auto'
-        type='text'
-        name='search'
-        placeholder='search todos'
+    <form className="search" onSubmit={handleSubmitFrom}>
+      <InputField
+        label=""
+        type="text"
+        name="search"
+        id="searchResult"
+        value={state.title}
+        placeholder="search todos"
+        onChange={onChange}
       />
     </form>
-  )
+  );
 }
 
 export default SearchFrom;
